12874b4cef2bd10de8b453a57ab61fbd5cd665d5
1298743328szyasy <1298743328@qq.com>
2018-11-09 19:45:19 +0800

    rthf

--- 计算器/ViewController.swift
+++ 计算器/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel?
+    var userIsInTheMiddleOfTyping = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
     }
-
-
+    
 }
 
